96265ea9b2f526a81614ff437969cb947b960a3a
Kamilsob <kamil.sobczak6@gmail.com>
2018-05-10 15:46:17 +0200

    Refactoring, changing tags_list to tags_str

--- classes/db_connection.py
+++ classes/db_connection.py
@@ -66,122 +66,61 @@ class DBConnection:
         if column_name is None: column_name =self.db_config['json']
 
         print(column_name)
+
         query = "SELECT "+column_name+" "
         query += "FROM "+table_name+" "
         query += "LIMIT 1 "
-        print(query)
-        self.execute_query(query)
 
+        print(query)
+
+        self.execute_query(query)
         row = self.cursor.fetchone()
 
         for item in row:
             json_dic =json.loads(''.join(item))
 
-
-
-        #json_list=
-
         json_list = []
-        # print("title:"+json_dic['title'])
+
         json_list.append(json_dic['title'])
 
-        # print("title_details:"+json_dic['title_detail']['base'])
         json_list.append(json_dic['title_detail']['base'])
 
-        # print("link:"+json_dic['link'])
         json_list.append(json_dic['link'])
 
         time_list=[]
         for item in json_dic['published_parsed'][0:6]:
             time_list.append(item)
-        # print(time_list)
         date_published =datetime.datetime(*map(int,time_list))
-        # print(date_published)
         json_list.append(date_published)
 
         tags_list=[]
         for item in json_dic['tags']:
             tags_list.append(item['term'])
-        # print(tags_list)
-        json_list.append(tags_list)
+        tags_str=','.join(tags_list)
+        json_list.append(tags_str)
 
-        # print("author:"+json_dic['author'])
         json_list.append(json_dic['author'])
 
-        # print("summary:"+json_dic['summary'])
         json_list.append(json_dic['summary'])
 
-        pprint.pprint(json_list[0:6])
+        #Check the json_list without 'summary' and it's length
+        #pprint.pprint(json_list[0:6])
         #print(json_list.__len__())
+        
         return json_list
 
     #CONVERTING JSON FILES TO INSERT_SQL QUERY
     def json_to_sql_values(self,json_dic):
-        sql_values = []
-
-        #TODO REFACTORING
-        # a="text"
-        # b="content"
-        # c="data"
-
-        sql_values.append(json_dic["id"])
-        sql_values.append(json_dic["text"]["content"]["data"]["username"])
-        sql_values.append(json_dic["text"]["content"]["data"]["text"])
-        sql_values.append(datetime.strptime(json_dic["text"]["content"]["data"]["time"], "%a %b %d %H:%M:%S %Y %Z"))
-        sql_values.append(json_dic["text"]["channel"])
-        sql_values.append(json_dic["text"]["content"]["data"]["room_id"])
-        sql_values.append(json_dic["text"]["content"]["data"]["symbol"])
-        sql_values.append(json_dic["text"]["content"]["data"]["user_id"])
-
-
-
-        return sql_values
+        print("hyy")
 
     #TODO INSERTING TO TABLE
     def insert_into_table(self,sql_values, columns="",table_name=None):
-
-        if columns=="":
-            columns= self.get_column_names()
-
-        if table_name is None:
-            table_name  = self.db_config['table']
-        # print(sql_values)
-        # print(columns)
-
-        for (i,item) in enumerate(sql_values):
-
-             if not isinstance(item,str) and not isinstance(item,datetime):
-                 sql_values[i]= str(item)
-
-        for item in sql_values:
-            print(type(item))
-
-        # toString = ",".join(sql_values)
-        # print(toString)
-
-        query = "INSERT INTO "+table_name+" "
-        #KOLUMNY
-        #query +="("+columns+") "
-        query +="VALUES(" + sql_values[0] + ")"
-        print(query)
-
-        # try:
-        #     self.cursor.execute(query)
-        #     self.cnx.commit()
-        #     print("COMMITED")
-        # except:
-        #     self.cnx.rollback()
-        #     print("ROLLBACK")
+        print("temp")
 
     #TODO CREATING QUERY
     # BROKEN DONT USE
     def create_query(self):
-        self.query = input("Insert MySQL query")
-        print(self.query)
-        self.cursor.execute(self.query)
-        for a in self.cursor:
-            answer_arr = json.lods(''.join(a))
-        self.query = answer_arr
+        print("benis")
 
     #EXECUTIN QUERY
     def execute_query(self, query=""):
@@ -196,36 +135,7 @@ class DBConnection:
             self.cnx.rollback()
             print("failure.")
 
-    #OLD DONT USE
-    #OPENNING CONNECTION TO DB
-    #USE CONNECTION_CONFIG TO CHANGE VARS
-    # def connect_to_db(self):
-    #
-    #     try:
-    #         cnx = mysql.connector.connect(**self.connection_config)
-    #
-    #     except mysql.connector.Error as err:
-    #         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    #             print("Something is wrong with your user name or password")
-    #         elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    #             print("Database does not exist")
-    #         else:
-    #             print(err)
-    #
-    #     else:
-    #         print("mysql connected")
-    #         cursor = cnx.cursor()
-    #         print("cursor created")
-    #
-    #         raw_json_val = self.get_raw_json()
-    #         print(raw_json_val)
-    #
-    #         # TEST
-    #         # for item in cursor
-    #
-    #         cursor.close()
-    #         print("cursor closed")
-    #         cnx.close()
+
 
     def __init__(self):
         self.connection_config = self.get_connection_config('../connection_config.txt')
