a1a0eaf281e2bb3f89222b1120d877abfff32abe
Kamilsob <kamil.sobczak6@gmail.com>
2018-05-11 15:22:30 +0200

    Refactoring

--- classes/db_connection.py
+++ classes/db_connection.py
@@ -6,20 +6,16 @@ from mysql.connector import errorcode
 
 
 class DBConnection:
-
-    #CONNECTION CONFIG IN connection_config.txt
-    #CONFIG TO ACCESS TABLES
-    db_config={
-        #DEFAULT
-        'table': 'rss_medium_sample',
-        'article_base': 'article_base',
-        'rss_medium': 'rss_medium'
-
-
+    # CONNECTION CONFIG IN connection_config.txt
+    # CONFIG TO ACCESS TABLES
+    db_config = {
+        # DEFAULT
+        'default': 'rss_medium_sample',
+        'article base': 'article_base',
+        'rss medium': 'rss_medium'
     }
 
-
-    def get_connection_config(self,loc):
+    def get_connection_config(self, loc):
         with open(loc, 'r') as f:
             read_data = []
             for line in f:
@@ -31,20 +27,18 @@ class DBConnection:
         connection_config = dict(zip(read_data[::2], read_data[1::2]))
         return connection_config
 
-    #GET COLUMNS FROM TABLE
-    #RETURNS LIST
-    def get_column_names(self,table_name=None,db_name=None):
+    # GET COLUMNS FROM TABLE
+    # RETURNS LIST
+    def get_column_names(self, table_name=None, db_name=None):
         print("in get_column_names")
 
-        if table_name is None:
-            table_name=self.db_config['table']
-        if db_name is None:
-            db_name=self.db_config['article_base']
+        if table_name is None: table_name = self.db_config['table']
+        if db_name is None: db_name = self.db_config['article base']
 
         query = " SELECT `COLUMN_NAME` "
         query += "FROM `INFORMATION_SCHEMA`.`COLUMNS` "
-        query += "WHERE `TABLE_SCHEMA`='"+db_name+"'"
-        query += "AND `TABLE_NAME`='"+table_name+"';"
+        query += "WHERE `TABLE_SCHEMA`='" + db_name + "'"
+        query += "AND `TABLE_NAME`='" + table_name + "';"
         column_names = []
 
         self.execute_query(query)
@@ -55,20 +49,20 @@ class DBConnection:
         for item in self.cursor:
             column_names.append(''.join(item))
 
-        #Returns list
+        # Returns list
         return column_names
 
-    #GETTING JSON RAW FILE FROM DB
-    #RETURNS DICTIONARY
+    # GETTING JSON RAW FILE FROM DB
+    # RETURNS LIST
     def get_raw_json(self, table_name=None, column_name=None):
 
         if table_name is None: table_name = self.db_config['table']
-        if column_name is None: column_name =self.db_config['json']
+        if column_name is None: column_name = self.db_config['json']
 
         print(column_name)
 
-        query = "SELECT "+column_name+" "
-        query += "FROM "+table_name+" "
+        query = "SELECT " + column_name + " "
+        query += "FROM " + table_name + " "
         query += "LIMIT 1 "
 
         print(query)
@@ -77,7 +71,7 @@ class DBConnection:
         row = self.cursor.fetchone()
 
         for item in row:
-            json_dic =json.loads(''.join(item))
+            json_dic = json.loads(''.join(item))
 
         json_list = []
 
@@ -87,60 +81,97 @@ class DBConnection:
 
         json_list.append(json_dic['link'])
 
-        time_list=[]
+        time_list = []
         for item in json_dic['published_parsed'][0:6]:
             time_list.append(item)
-        date_published =datetime.datetime(*map(int,time_list))
+        date_published = datetime.datetime(*map(int, time_list))
         json_list.append(date_published)
 
-        tags_list=[]
+        tags_list = []
         for item in json_dic['tags']:
             tags_list.append(item['term'])
-        tags_str=','.join(tags_list)
+        tags_str = ','.join(tags_list)
         json_list.append(tags_str)
 
         json_list.append(json_dic['author'])
 
         json_list.append(json_dic['summary'])
+        #TEST
+        # Check the json_list without 'summary' and it's length
+        # pprint.pprint(json_list[0:6])
+        # print(json_list.__len__())
 
-        #Check the json_list without 'summary' and it's length
-        #pprint.pprint(json_list[0:6])
-        #print(json_list.__len__())
-        
         return json_list
 
-    #CONVERTING JSON FILES TO INSERT_SQL QUERY
-    def json_to_sql_values(self,json_dic):
+    # CONVERTING JSON FILES TO INSERT_SQL QUERY
+    def json_to_sql_values(self, json_dic):
         print("hyy")
+    #
+    # TODO INSERTING TO TABLE
+    def insert_query(self, query=None, table=None):
 
-    #TODO INSERTING TO TABLE
-    def insert_into_table(self,sql_values, columns="",table_name=None):
-        print("temp")
+        #Get Column names from table
+        if table is None:
+            columns = self.get_column_names(table_name='structured_data')
+        else:
+            columns = self.get_column_names(table_name=table)
+        #print(columns)
 
-    #TODO CREATING QUERY
-    # BROKEN DONT USE
+        #query_temp = "INSERT INTO `" + table + "`("
+        for item in columns:
+            print(item)
+
+        query = """  INSERT INTO
+        `structured_data`(`id_structured_data`, 
+        `id_rss_medium`, 
+        `time_created`, 
+        `topic`,
+        `author`, 
+        `main_text`, 
+        `tags`,
+        `links`, 
+        `link_hub`,
+        `html`,
+        `summary`)
+        VALUES(
+        null,
+         124,
+        '2011-06-06 13:15:20',
+        'raz',
+        'peja',
+        'dlugitext',
+        'tekst,teskt',
+        'http://ayy.com',
+        'szto',
+        '<html></html>', 
+        'traeszakfg'
+        )
+        ;    
+        """
+        self.execute_query(query)
+
+    # TODO CREATING QUERY
     def create_query(self):
         print("benis")
 
-    #EXECUTIN QUERY
+    # EXECUTIN QUERY
     def execute_query(self, query=""):
-        if query=="":
+        if query == "":
             query = self.query
         print("executing")
         try:
             self.cursor.execute(query)
             self.cnx.commit()
             print("sucess.")
-        except:
-            self.cnx.rollback()
+        except mysql.connector.Error as err:
+            print(self.cnx.rollback())
+            print(err)
             print("failure.")
-
-
 
     def __init__(self):
         self.connection_config = self.get_connection_config('../connection_config.txt')
         self.cnx = mysql.connector.connect(**self.connection_config)
-        self.cursor = self.cnx.cursor(buffered =True)
+        self.cursor = self.cnx.cursor(buffered=True)
         self.query = ""
 
     def __enter__(self):
@@ -150,13 +181,14 @@ class DBConnection:
         if self.cnx:
             self.cnx.close()
 
+
 def main():
     print("in main")
     db = DBConnection()
 
-    #column_names= db.get_column_names(table_name='rss_medium_sample')
-
-    db.get_raw_json(column_name="json")
+    # column_names= db.get_column_names(table_name='rss_medium_sample')
+    db.insert_query('')
+    # db.get_raw_json(column_name="json")
 
 
 if __name__ == "__main__":
