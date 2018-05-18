1a1cc240a4551e498165875197e76d6d7679efc2
Kamilsob <kamil.sobczak6@gmail.com>
2018-05-18 19:59:05 +0200

    jupyter notebook, text_cleaner
    added jupyter notebook for text cleaning

--- classes/text_cleaner.py
+++ classes/text_cleaner.py
@@ -3,23 +3,46 @@ import io
 import re
 text=""
 new_text=[]
+
 new_string=""
 f_name = "Binance Flash Update!"
 f_name = "../main_text/" + f_name + ".txt"
 
 with io.open(f_name, "r", encoding="utf-8") as f:
     text = f.read()
+
 print(text)
+
+# text.translate(None,text.punctuation)
+
+
+# for char in text:
+#     print(char, char.isalnum(),char in str.)
+#     if not char.isalnum():
+#         char = " "
+#     new_text.append(char)
+#
+# new_string=''.join(new_text)
+
 # removing links
 # slabo dziala re_pattern_link ='/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g'
-re_pattern_link_new='(www|http:|https:)+[^\s]+[\w]'
+
 #po_re = re.search(re_pattern_link,text)
 #print(po_re)
-
+#TODO
+#TOCONSIDER
+#TOREFACTOR
+#TEST
+#FIXME
+re_pattern_link ='/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g'
+re_pattern_link_new='(www|http:|https:)+[^\s]+[\w]'
 po_re =re.search(re_pattern_link_new,text)
-po_re_many = re.findall(re_pattern_link_new,text)
-print(po_re_many)
-print(po_re.group(0))
+po_re = re.finditer(re_pattern_link_new,text)
+for x in po_re:
+    print(po_re.__next__())
+
+
+str.__len__()
 # for i,v  in enumerate(po_re_many):
     # print(po_re_many.group(i))
 # text =text.__repr__()
@@ -28,13 +51,7 @@ print(po_re.group(0))
 # print(test)
 
 #remove not digit not alhaphbets words:
-# for char in text:
-#     #print(char, char.isalnum())
-#     if not char.isalnum():
-#         char = " "
-#     new_text.append(char)
-#
-# new_string=''.join(new_text)
+
 #
 # words = new_string.split(sep=" ")
 # #for word in words:
